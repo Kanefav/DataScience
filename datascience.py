9f85825a12f55fd4bf035be45378a6f05d640c5c
Kanefav <gabrielfav12@gmail.com>
2021-07-08 14:02:54 -0300

    pandas

--- datascience.py
+++ datascience.py
@@ -19,4 +19,3 @@ print(f'Media: {Series.mean():.2f}') # media das notas, .min() nota minima, .max
 print(DataFrame.describe()) # Tabela com informaçoes uteis sobre o data frame
                           # Mediana = valor do meio que divide uma porcentagem 
                 # exemplo: 50% (ganham menos que a mediana)  |Mediana|  (50% ganham mais que a mediana)
-
